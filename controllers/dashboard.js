eacac89cf747df0aae538f95f5cbfe6eb01adb87
Glitch (heather-muddy-lamprey) <none>
2022-08-28 12:17:24 +0000

    🐷💐 Checkpoint ./views/partials/listreadings.hbs:47481140/161 ./controllers/dashboard.js:47481140/422 ./views/dashboard.hbs:47481140/61

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,12 +2,31 @@
 
 const logger = require("../utils/logger");
 
+const stationsList = {
+  title: 'Tramore',
+  readings: [
+    {
+      code: 800,
+      temp: 0.5,
+      windSpeed: 2.0
+    },
+     {
+      code: 900,
+      temp: 0.5,
+      windSpeed: 6.0
+    }
+    
+  ]
+}
+
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "Weathertop Dashboard",
+      stations: stationsList
     };
+    logger.info('a')
     response.render("dashboard", viewData);
   },
 };
